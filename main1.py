5579a659d935f4749b29dfd1e14738e989ab8402
Muhammad Zaid <guyspakistan@gmail.com>
2024-11-07 20:06:50 +0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,8 @@
+class vechiel:
+    def __init__(self, maxspeed, mileage):
+        self.maxspeed = maxspeed
+        self.mileage = mileage
+        
+modelx = vechiel(220, 2)
+print('the modelx maxspeed is' , modelx.maxspeed )
+print('the modelx mileage is', modelx.mileage)
